a910dfa50351bff87a0ad7ebd098e786e24260c4
JASON YOUNG <moorslf@gmail.com>
2023-03-14 14:40:52 -0400

    Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideFeet){
   // Write your code here!
+  if (rideFeet < 400) {
+    return 'This one is on me!'
+  } else if (rideFeet >= 400 && rideFeet <= 2000) {
+      return 'That will be twenty bucks.'
+  } else if (rideFeet > 2000 && rideFeet < 2500) {
+      return 'I will gladly take your thirty bucks.'
+  } else if (rideFeet > 2500) {
+    return `No can do.`
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return `Thank you so much.`
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return 'Bye.'
+  }
 }
